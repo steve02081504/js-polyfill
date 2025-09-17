e7d0b286afa2e188bd82b6c927781a71897b5082
steve02081504 <steve02081504@EDEN.fukwold>
2025-09-17 10:22:04 +0800

    esm.run -> esm.sh

--- browser.mjs
+++ browser.mjs
@@ -1,12 +1,12 @@
 if (!('anchorName' in document.documentElement.style))
-	import('https://esm.run/@oddbird/css-anchor-positioning').catch(_ => 0)
+	import('https://esm.sh/@oddbird/css-anchor-positioning').catch(_ => 0)
 
 if (!('popover' in HTMLElement.prototype))
-	await import('https://esm.run/@oddbird/popover-polyfill').catch(_ => 0)
+	await import('https://esm.sh/@oddbird/popover-polyfill').catch(_ => 0)
 
 if (!navigator?.plugins?.['Shockwave Flash'])
-	await import('https://esm.run/@ruffle-rs/ruffle').catch(_ => 0)
+	await import('https://esm.sh/@ruffle-rs/ruffle').catch(_ => 0)
 
 const scripts = Array.from(document.querySelectorAll('script'))
 if (scripts.some(script => script.src?.includes?.('@tailwindcss/browser')))
-	import('https://esm.run/@unocss/runtime')
+	import('https://esm.sh/@unocss/runtime')
