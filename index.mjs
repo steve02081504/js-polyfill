4deeae96242eca8aa8e96750f7f589b786b3efb9
steve green <steve_green@qq.com>
2026-02-10 12:51:22 +0800

    polyfill as needed

--- index.mjs
+++ index.mjs
@@ -1,3 +1,3 @@
-await import('https://esm.sh/core-js').catch(_ => 0)
+if (!JSON.isRawJSON) await import('https://esm.sh/core-js').catch(_ => 0)
 
 if (globalThis.document) await import('./browser.mjs').catch(_ => 0)
